a39a1345e34f8c62dbed775dafbb4a377f7114bb
hcort <elpadawan@gmail.com>
2023-10-20 23:34:15 +0200

    merge

--- get_youtube_audio.py
+++ get_youtube_audio.py
@@ -1,6 +1,10 @@
+import os
+
 import pytube as pytube
 import requests
-from mp3_tags import write_id3_tags_dict
+from slugify import slugify
+
+from mp3_tags import mp4_to_mp3, write_id3_tags_dict
 
 
 def save_image_from_url(thumbnail_url, output_path, nombre):
@@ -36,8 +40,8 @@ def youtube_to_mp3(output_path, url):
             'date': str(yt.publish_date),
             'length': yt.length,
             'website': yt.channel_url,
-            'comment': f'{url}\n{yt.channel_url}\n{yt.description}',
-            'description': f'{url}\n{yt.channel_url}\n{yt.description}',
+            # 'comment': f'{url}\n{yt.channel_url}\n{yt.description}',
+            # 'description': f'{url}\n{yt.channel_url}\n{yt.description}',
             'genre': 'Podcast'
         }
         from slugify import slugify
@@ -46,7 +50,57 @@ def youtube_to_mp3(output_path, url):
         stream.download(output_path=output_path, filename=f'{nombre}.{extension}')
         cover_image_filename = save_image_from_url(yt.thumbnail_url, output_path, nombre)
         mp3_filename = mp4_to_mp3(output_path, nombre, extension)
-        write_id3_tags_dict(mp3_filename, cover_image_filename, tag_dict)
+        write_id3_tags_dict(mp3_filename=mp3_filename, art_filename=None, tag_dict=tag_dict)
+    except pytube.exceptions.RegexMatchError:
+        print("URL no encontrada")
+    except BaseException as err:
+        print(f"Unexpected {err}, {type(err)}")
+    except Exception as err:
+        print(f"Unexpected {err}, {type(err)}")
+
+
+def list_videos_from_playlist(playlist_url):
+    return [video.watch_url for video in pytube.Playlist(playlist_url).videos]
+
+
+def create_output_folder_playlist(playlist_url, output_root):
+    pl = pytube.Playlist(playlist_url)
+    normalized_name = slugify(f'{pl.owner}-{pl.title}')
+    full_path = os.path.join(output_root, normalized_name)
+    if not os.path.exists(full_path):
+        os.makedirs(full_path)
+    return full_path
+
+
+def youtube_download_video(output_path, url, index=0):
+    try:
+        yt = pytube.YouTube(url)
+        video_stream = yt.streams.filter(mime_type='video/mp4', res="1080p").desc().first()
+        video_stream.download(
+            output_path=output_path, filename='temp_video.mp4')
+        import moviepy.editor
+
+        # Download audio and rename
+        audio_stream = pytube.YouTube(url).streams.filter(only_audio=True).desc().first().download(
+            output_path=output_path, filename='temp_audio.mp4'
+        )
+
+        # Setting the audio to the video
+        video = moviepy.editor.VideoFileClip(os.path.join(output_path, 'temp_video.mp4'))
+        audio = moviepy.editor.AudioFileClip(os.path.join(output_path, 'temp_audio.mp4'))
+        final = video.set_audio(audio)
+
+        # Output result
+
+        # Delete video and audio to keep the result
+        from slugify import slugify
+        nombre = slugify(video_stream.default_filename[:-4])
+        extension = video_stream.default_filename[-3:]
+        final_filename = os.path.join(output_path, f'{index} - {nombre}.{extension}')
+        final.write_videofile(final_filename)
+
+        os.remove(os.path.join(output_path, 'temp_video.mp4'))
+        os.remove(os.path.join(output_path, 'temp_audio.mp4'))
     except pytube.exceptions.RegexMatchError:
         print("URL no encontrada")
     except BaseException as err:
