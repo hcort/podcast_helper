e7251744edcc157198691648cbac3d857d245293
hcort <elpadawan@gmail.com>
2024-12-02 09:40:00 +0100

    Finer file handling

--- utils.py
+++ utils.py
@@ -65,15 +65,29 @@ def zip_folder(base_folder, folder):
     return f'{filename}.zip'
 
 
-def create_filename_and_folders(output_dir, podcast_title, filename):
+def get_download_folder(output_dir, podcast_title):
     if not output_dir:
-        output_dir = 'temp_output_dir'
-    dir_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), output_dir)
+        dir_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'temp_output_dir')
+    else:
+        dir_path = output_dir
     if not os.path.exists(dir_path):
         os.makedirs(dir_path)
     dir_path = os.path.join(dir_path, podcast_title)
     if not os.path.exists(dir_path):
         os.makedirs(dir_path)
+    return dir_path
+
+
+def create_filename_and_folders(output_dir, podcast_title, filename):
+    # if not output_dir:
+    #     output_dir = 'temp_output_dir'
+    # dir_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), output_dir)
+    # if not os.path.exists(dir_path):
+    #     os.makedirs(dir_path)
+    # dir_path = os.path.join(dir_path, podcast_title)
+    # if not os.path.exists(dir_path):
+    #     os.makedirs(dir_path)
+    dir_path = get_download_folder(output_dir, podcast_title)
     return os.path.join(dir_path, slugify(filename, max_length=200))
 
 
